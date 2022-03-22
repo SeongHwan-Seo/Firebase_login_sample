f0b429d879c142a3dd1b8deccef0cf913759f180
seosh5704 <seosh5704@gmail.com>
2022-03-22 19:56:16 +0900

    Initial Commit

---
+++ SfotifyLoginSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SfotifyLoginSampleApp
+//
+//  Created by seosh on 3/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
